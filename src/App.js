4598dcb5730fe07742b134915088e61203266abd
kundan kumar <kundan.kumar@kuliz.com>
2020-09-12 12:13:07 +0530

    initial page added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h3>AmzWork this website under constructon !!</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
